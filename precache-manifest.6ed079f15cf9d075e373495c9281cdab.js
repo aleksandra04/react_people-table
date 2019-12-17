1ee93a927f60d04dcc754fe71fc4212824ee4343
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-17 15:45:07 +0200

    Updates

---
+++ precache-manifest.6ed079f15cf9d075e373495c9281cdab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ad446123d7186e1853fe6e95528e82",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "f7dba0add8e1594b8ccb",
+    "url": "/react_people-table/static/css/main.03b9aaff.chunk.css"
+  },
+  {
+    "revision": "8c62e9e39566e04c3c1e",
+    "url": "/react_people-table/static/js/2.263e448f.chunk.js"
+  },
+  {
+    "revision": "f7dba0add8e1594b8ccb",
+    "url": "/react_people-table/static/js/main.e65ed420.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
